454bfd5c68e5696912c8d3d8b26e1b1e5c4f5074
fengzhizi715 <fengzhizi715@126.com>
2018-06-11 20:47:31 +0800

    update PrefsFastJSON.kt

--- saf-prefs-fastjson-delegates/src/main/java/com/safframework/delegate/PrefsFastJSON.kt
+++ saf-prefs-fastjson-delegates/src/main/java/com/safframework/delegate/PrefsFastJSON.kt
@@ -2,6 +2,8 @@ package com.safframework.delegate
 
 import android.content.SharedPreferences
 import com.alibaba.fastjson.JSON
+import com.alibaba.fastjson.JSONObject
+import com.alibaba.fastjson.serializer.SerializerFeature
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
@@ -23,7 +25,20 @@ inline fun <reified T> SharedPreferences.json(key: String? = null, defaultValue:
                 return if (s.isBlank()) defaultValue else JSON.parseObject(s, T::class.java)
             }
 
-            override fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
-                edit().putString(key ?: property.name, JSON.toJSONString(value)).apply()
+            override fun setValue(thisRef: Any, property: KProperty<*>, value: T) =
+                    edit().putString(key ?: property.name, JSON.toJSONString(value)).apply()
+        }
+
+inline fun <reified T> SharedPreferences.jsonList(key: String? = null) =
+        object : ReadWriteProperty<Any, List<T>> {
+
+            override fun getValue(thisRef: Any, property: KProperty<*>): List<T> {
+
+                val s = getString(key ?: property.name, "")
+                return if (s.isBlank()) emptyList() else JSON.parseArray(s,T::class.java)
+            }
+
+            override fun setValue(thisRef: Any, property: KProperty<*>, value: List<T>) {
+                edit().putString(key ?: property.name, JSONObject.toJSONString(value, SerializerFeature.WriteClassName)).apply()
             }
         }
